ac23f9ce946cacc4623e2b36311acbca296da3b3
Michael Heffernan <michael.heffernan.j@gmail.com>
2021-10-22 18:39:26 -0400

    updated database env variable

--- server.js
+++ server.js
@@ -11,11 +11,8 @@ const image = require("./controllers/image")
 const db = knex({
   client: "pg",
   connection: {
-    host: "postgresql-clear-83795",
-    port: 5432,
-    user: "michaelheffernan",
-    password: "",
-    database: "smart-brain"
+    host: process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
