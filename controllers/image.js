83a555d608dcb82831c50601a8d0ee85af14a0d8
Michael Heffernan <michael.heffernan.j@gmail.com>
2021-10-21 21:19:41 -0400

    initial commit

---
+++ controllers/image.js
@@ -0,0 +1,34 @@
+const clarifai = require('clarifai')
+
+const app = new Clarifai.App({
+    apiKey: "4655685b3f4f463cbf38a6fe4bec7781"
+  });
+
+  const handleApiCall = (req, res) => {
+    app.models
+    .predict(
+      Clarifai.FACE_DETECT_MODEL,
+      // THE JPG
+      req.body.input
+    ).then(data => {
+        res.json(data)
+    })
+    .catch(err => res.status(400).json("unable to work with api"))
+  }
+ 
+
+const handleImage = (req, res, db) => {
+    const { id } = req.body;
+    db('users').where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+      res.json(entries[0]);
+    })
+    .catch(err => res.status(400).json('unable to get entries'))
+  }
+  
+  module.exports = {
+    handleImage,
+    handleApiCall
+  }
