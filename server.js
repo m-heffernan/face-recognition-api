557693cd49658ab78a661c6465f225aa2c608c9d
Michael Heffernan <michael.heffernan.j@gmail.com>
2021-10-22 18:34:30 -0400

    updating database for heroku

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const image = require("./controllers/image")
 const db = knex({
   client: "pg",
   connection: {
-    host: "127.0.0.1",
+    host: "postgresql-clear-83795",
     port: 5432,
     user: "michaelheffernan",
     password: "",
